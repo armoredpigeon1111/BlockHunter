19b487b88b7caa84151196a2bfc3c0c30d86d3e3
leotis.fields <leotis.fields@gmail.com>
2019-05-06 21:26:34 -0400

    Added comments to SearchActivity

--- app/src/main/java/com/example/blockhunter/SearchActiviy.java
+++ app/src/main/java/com/example/blockhunter/SearchActiviy.java
@@ -1,5 +1,5 @@
 //Class Project
-//Author of Search Page: Leotis Fields
+//Author of Search Page: Leotis M Fields
 
 package com.example.blockhunter;
 import android.support.v7.app.AppCompatActivity;
